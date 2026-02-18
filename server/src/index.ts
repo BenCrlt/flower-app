ef8949a1b03f624ab14fe5f56f0cdb16f101ef0a
benoit.cournault <benoit.cournault@polyconseil.fr>
2026-02-18 15:47:11 +0100

    feat: set up drizzle and first migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,9 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 import Fastify from "fastify";
 import mercurius from "mercurius";
+
+const db = drizzle(process.env.DATABASE_URL!);
 
 const app = Fastify({
   logger: true,
